87c4ea988440d733534bbacfb69f3cb0ac7a849d
Robin Karlsson <robin.karlsson@csc.fi>
2024-08-23 11:59:41 +0300

    Add separator between nav-links

--- form.js
+++ form.js
@@ -28,7 +28,7 @@ function setup_extra_packages_field() {
   form_group.parent().addClass("advanced").css("margin-bottom", "0");
   form_group.parent().css("margin-bottom", "0rem");
   inputs.css("opacity", "0");
-  divs.removeClass("form-check").addClass("nav-link");
+  divs.removeClass("form-check").addClass("nav-link").css("border-left", "rgb(51, 122, 183) 1px solid");
   const pill_div = $("<div></div>").css("display", "flex").css("flex-wrap", "nowrap");
   pill_div.append(divs);
   form_group.append(pill_div);
